00c47c5240bf470113a2b5961abfcb467429062a
bobbythree <mynameisbobbylewis@hotmail.com>
2024-12-22 15:07:13 -0600

    code cleanup and comments

--- main.js
+++ main.js
@@ -1,23 +1,20 @@
+//set up canvas and 2d context
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-
+//wave properties
 const wave = {
   y: canvas.height / 2,
   length: 0.02,
   frequency: 0.008,
   amplitude: 100 
 }
-
 let increment = wave.frequency;
 
+// set line color. draw horizontal line at center of page. define wave logic
 function drawWave() {
-  ctx.shadowOffsetX = 0;
-  ctx.shadowOffsetY = 0;
-  ctx.shadowColor = '';
-  ctx.shadowBlur = 0;
   ctx.strokeStyle = 'black'   
   ctx.beginPath();
   ctx.moveTo(0, wave.y);
@@ -27,6 +24,7 @@ function drawWave() {
   ctx.stroke();    
 }
 
+// animate wave
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   drawWave();
